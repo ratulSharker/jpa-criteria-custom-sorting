c5234409d09b68cd8dc9de9cc6e5b43b2fbcc7c3
Ratul Sharker <sharker.ratul.08@gmail.com>
2024-08-06 01:52:55 +0600

    initial project setup.

---
+++ CustomSorting/src/main/java/com/example/CustomSorting/CustomSortingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomSorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSortingApplication.class, args);
+	}
+
+}
---
+++ CustomSorting/src/test/java/com/example/CustomSorting/CustomSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomSorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
